1f1089bf8c9811138b827699243fecddc63d131f
omena88 <105069963+omena88@users.noreply.github.com>
2024-11-28 00:30:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ import pandas as pd
 import streamlit as st
 
 # Show app title and description.
-st.set_page_config(page_title="Support tickets", page_icon="🎫")
-st.title("🎫 Support tickets")
+st.set_page_config(page_title="Tickets de soporte", page_icon="🎫")
+st.title("🎫 Tickets de soporte")
 st.write(
     """
     This app shows how you can build an internal tool in Streamlit. Here, we are 
